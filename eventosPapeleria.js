55974e60e9761842fe1cf1ace71f01d377db81a0
Santiago1470 <67245823+Santiago1470@users.noreply.github.com>
2022-01-10 21:51:00 -0500

    Add files via upload

---
+++ eventosPapeleria.js
@@ -0,0 +1,76 @@
+var clic = 1;
+
+function abrirDetalles() {
+  if (clic == 1) {
+    cer = document.getElementById('cerrar');
+    abr = document.getElementById('abrir');
+    det = document.getElementById('texto1');
+    lin = document.getElementById('linea');
+    abr.style.display = 'none';
+    cer.style.display = 'block';
+    det.style.display = 'block';
+    lin.style.display = 'block';
+    clic = clic + 1;
+
+  } else {
+    cer = document.getElementById('cerrar');
+    abr = document.getElementById('abrir');
+    det = document.getElementById('texto1');
+    lin = document.getElementById('linea');
+    abr.style.display = 'block';
+    cer.style.display = 'none';
+    det.style.display = 'none';
+    lin.style.display = 'none';
+    clic = 1;
+  }
+}
+
+function abrirDetalles2() {
+  if (clic == 1) {
+    cer = document.getElementById('cerrar2');
+    abr = document.getElementById('abrir2');
+    det = document.getElementById('texto2');
+    lin = document.getElementById('linea2');
+    abr.style.display = 'none';
+    cer.style.display = 'block';
+    det.style.display = 'block';
+    lin.style.display = 'block';
+    clic = clic + 1;
+
+  } else {
+    cer = document.getElementById('cerrar2');
+    abr = document.getElementById('abrir2');
+    det = document.getElementById('texto2');
+    lin = document.getElementById('linea2');
+    abr.style.display = 'block';
+    cer.style.display = 'none';
+    det.style.display = 'none';
+    lin.style.display = 'none';
+    clic = 1;
+  }
+}
+
+function abrirDetalles3() {
+  if (clic == 1) {
+    cer = document.getElementById('cerrar3');
+    abr = document.getElementById('abrir3');
+    det = document.getElementById('texto3');
+    lin = document.getElementById('linea3');
+    abr.style.display = 'none';
+    cer.style.display = 'block';
+    det.style.display = 'block';
+    lin.style.display = 'block';
+    clic = clic + 1;
+
+  } else {
+    cer = document.getElementById('cerrar3');
+    abr = document.getElementById('abrir3');
+    det = document.getElementById('texto3');
+    lin = document.getElementById('linea3');
+    abr.style.display = 'block';
+    cer.style.display = 'none';
+    det.style.display = 'none';
+    lin.style.display = 'none';
+    clic = 1;
+  }
+}
