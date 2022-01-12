2b1653da2e93328fddaf4e97871218ef0ec6efa0
Santiago1470 <67245823+Santiago1470@users.noreply.github.com>
2022-01-11 20:28:45 -0500

    Add files via upload

--- eventosPapeleria.js
+++ eventosPapeleria.js
@@ -1,76 +1,150 @@
-var clic = 1;
+let detalles1 = document.getElementById('detalles1');
+let detalles2 = document.getElementById('detalles2');
+let detalles3 = document.getElementById('detalles3');
 
-function abrirDetalles() {
-  if (clic == 1) {
-    cer = document.getElementById('cerrar');
-    abr = document.getElementById('abrir');
-    det = document.getElementById('texto1');
-    lin = document.getElementById('linea');
-    abr.style.display = 'none';
-    cer.style.display = 'block';
-    det.style.display = 'block';
-    lin.style.display = 'block';
-    clic = clic + 1;
+let verDetalles1 = document.getElementById('verDetalles1');
+let verDetalles2 = document.getElementById('verDetalles2');
+let verDetalles3 = document.getElementById('verDetalles3');
 
-  } else {
-    cer = document.getElementById('cerrar');
-    abr = document.getElementById('abrir');
-    det = document.getElementById('texto1');
-    lin = document.getElementById('linea');
-    abr.style.display = 'block';
-    cer.style.display = 'none';
-    det.style.display = 'none';
-    lin.style.display = 'none';
-    clic = 1;
+let flecha1 = document.getElementById('flecha1');
+let flecha2 = document.getElementById('flecha2');
+let flecha3 = document.getElementById('flecha3');
+
+let x1 = document.getElementById('x1');
+let x2 = document.getElementById('x2');
+let x3 = document.getElementById('x3');
+
+let linea1 = document.getElementById('linea1');
+let linea2 = document.getElementById('linea2');
+let linea3 = document.getElementById('linea3');
+
+let texto1 = document.getElementById('texto1');
+let texto2 = document.getElementById('texto2');
+let texto3 = document.getElementById('texto3');
+
+detalles1.addEventListener('click', abrCerDetalles1);
+detalles2.addEventListener('click', abrCerDetalles2);
+detalles3.addEventListener('click', abrCerDetalles3);
+
+function abrCerDetalles1() {
+  flecha1.classList.toggle('ocultar');
+  x1.classList.toggle('mostrar');
+  linea1.classList.toggle('mostrar');
+  texto1.classList.toggle('mostrar');
+
+  if (flecha1.classList.contains('ocultar')) {
+    verDetalles1.innerHTML = 'CERRAR DETALLES';
+  }
+  else {
+    verDetalles1.innerHTML = 'VER DETALLES';
   }
 }
 
-function abrirDetalles2() {
-  if (clic == 1) {
-    cer = document.getElementById('cerrar2');
-    abr = document.getElementById('abrir2');
-    det = document.getElementById('texto2');
-    lin = document.getElementById('linea2');
-    abr.style.display = 'none';
-    cer.style.display = 'block';
-    det.style.display = 'block';
-    lin.style.display = 'block';
-    clic = clic + 1;
+function abrCerDetalles2() {
+  flecha2.classList.toggle('ocultar');
+  x2.classList.toggle('mostrar');
+  linea2.classList.toggle('mostrar');
+  texto2.classList.toggle('mostrar');
 
-  } else {
-    cer = document.getElementById('cerrar2');
-    abr = document.getElementById('abrir2');
-    det = document.getElementById('texto2');
-    lin = document.getElementById('linea2');
-    abr.style.display = 'block';
-    cer.style.display = 'none';
-    det.style.display = 'none';
-    lin.style.display = 'none';
-    clic = 1;
+  if (flecha2.classList.contains('ocultar')) {
+    verDetalles2.innerHTML = 'CERRAR DETALLES';
+  }
+  else {
+    verDetalles2.innerHTML = 'VER DETALLES';
   }
 }
 
-function abrirDetalles3() {
-  if (clic == 1) {
-    cer = document.getElementById('cerrar3');
-    abr = document.getElementById('abrir3');
-    det = document.getElementById('texto3');
-    lin = document.getElementById('linea3');
-    abr.style.display = 'none';
-    cer.style.display = 'block';
-    det.style.display = 'block';
-    lin.style.display = 'block';
-    clic = clic + 1;
+function abrCerDetalles3() {
+  flecha3.classList.toggle('ocultar');
+  x3.classList.toggle('mostrar');
+  linea3.classList.toggle('mostrar');
+  texto3.classList.toggle('mostrar');
 
-  } else {
-    cer = document.getElementById('cerrar3');
-    abr = document.getElementById('abrir3');
-    det = document.getElementById('texto3');
-    lin = document.getElementById('linea3');
-    abr.style.display = 'block';
-    cer.style.display = 'none';
-    det.style.display = 'none';
-    lin.style.display = 'none';
-    clic = 1;
+  if (flecha3.classList.contains('ocultar')) {
+    verDetalles3.innerHTML = 'CERRAR DETALLES';
+  }
+  else {
+    verDetalles3.innerHTML = 'VER DETALLES';
   }
 }
+
+
+
+
+
+// var clic = 1;
+
+// function abrirDetalles() {
+//   if (clic == 1) {
+//     cer = document.getElementById('cerrar');
+//     abr = document.getElementById('abrir');
+//     det = document.getElementById('texto1');
+//     lin = document.getElementById('linea');
+//     abr.style.display = 'none';
+//     cer.style.display = 'block';
+//     det.style.display = 'block';
+//     lin.style.display = 'block';
+//     clic = clic + 1;
+
+//   } else {
+//     cer = document.getElementById('cerrar');
+//     abr = document.getElementById('abrir');
+//     det = document.getElementById('texto1');
+//     lin = document.getElementById('linea');
+//     abr.style.display = 'block';
+//     cer.style.display = 'none';
+//     det.style.display = 'none';
+//     lin.style.display = 'none';
+//     clic = 1;
+//   }
+// }
+
+// function abrirDetalles2() {
+//   if (clic == 1) {
+//     cer = document.getElementById('cerrar2');
+//     abr = document.getElementById('abrir2');
+//     det = document.getElementById('texto2');
+//     lin = document.getElementById('linea2');
+//     abr.style.display = 'none';
+//     cer.style.display = 'block';
+//     det.style.display = 'block';
+//     lin.style.display = 'block';
+//     clic = clic + 1;
+
+//   } else {
+//     cer = document.getElementById('cerrar2');
+//     abr = document.getElementById('abrir2');
+//     det = document.getElementById('texto2');
+//     lin = document.getElementById('linea2');
+//     abr.style.display = 'block';
+//     cer.style.display = 'none';
+//     det.style.display = 'none';
+//     lin.style.display = 'none';
+//     clic = 1;
+//   }
+// }
+
+// function abrirDetalles3() {
+//   if (clic == 1) {
+//     cer = document.getElementById('cerrar3');
+//     abr = document.getElementById('abrir3');
+//     det = document.getElementById('texto3');
+//     lin = document.getElementById('linea3');
+//     abr.style.display = 'none';
+//     cer.style.display = 'block';
+//     det.style.display = 'block';
+//     lin.style.display = 'block';
+//     clic = clic + 1;
+
+//   } else {
+//     cer = document.getElementById('cerrar3');
+//     abr = document.getElementById('abrir3');
+//     det = document.getElementById('texto3');
+//     lin = document.getElementById('linea3');
+//     abr.style.display = 'block';
+//     cer.style.display = 'none';
+//     det.style.display = 'none';
+//     lin.style.display = 'none';
+//     clic = 1;
+//   }
+// }
